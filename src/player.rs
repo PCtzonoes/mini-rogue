613d1fe616344fa577fd371f19cb59dea4da7adc
Pedro <pedrop6@hotmail.com.br>
2022-08-22 15:10:43 -0700

    add wasd keys

--- src/player.rs
+++ src/player.rs
@@ -22,21 +22,25 @@ pub fn player_input(gs: &mut State, ctx: &mut Rltk) {
 	match ctx.key {
 		None => {} // Nothing happened
 		Some(key) => match key {
-			VirtualKeyCode::Left | VirtualKeyCode::Numpad4 | VirtualKeyCode::H => {
-				try_move_player(-1, 0, &mut gs.ecs)
-			}
+			VirtualKeyCode::Left
+			| VirtualKeyCode::Numpad4
+			| VirtualKeyCode::A
+			| VirtualKeyCode::H => try_move_player(-1, 0, &mut gs.ecs),
 
-			VirtualKeyCode::Right | VirtualKeyCode::Numpad6 | VirtualKeyCode::L => {
-				try_move_player(1, 0, &mut gs.ecs)
-			}
+			VirtualKeyCode::Right
+			| VirtualKeyCode::Numpad6
+			| VirtualKeyCode::D
+			| VirtualKeyCode::L => try_move_player(1, 0, &mut gs.ecs),
 
-			VirtualKeyCode::Up | VirtualKeyCode::Numpad8 | VirtualKeyCode::K => {
-				try_move_player(0, -1, &mut gs.ecs)
-			}
+			VirtualKeyCode::Up
+			| VirtualKeyCode::Numpad8
+			| VirtualKeyCode::W
+			| VirtualKeyCode::K => try_move_player(0, -1, &mut gs.ecs),
 
-			VirtualKeyCode::Down | VirtualKeyCode::Numpad2 | VirtualKeyCode::J => {
-				try_move_player(0, 1, &mut gs.ecs)
-			}
+			VirtualKeyCode::Down
+			| VirtualKeyCode::Numpad2
+			| VirtualKeyCode::S
+			| VirtualKeyCode::J => try_move_player(0, 1, &mut gs.ecs),
 
 			_ => {}
 		},
