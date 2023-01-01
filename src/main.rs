02d50c2372513af9743604015d5f24010addfc3c
Pedro <ppdamace@gmail.com>
2022-12-31 22:24:44 -0300

    basic monsters(no behavior)

--- src/main.rs
+++ src/main.rs
@@ -37,9 +37,11 @@ impl GameState for State {
 
 		let positions = self.ecs.read_storage::<Position>();
 		let renderables = self.ecs.read_storage::<Renderable>();
+		let map = self.ecs.fetch::<Map>();
 
 		for (pos, render) in (&positions, &renderables).join() {
-			ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
+			let idx = map.xy_idx(pos.x, pos.y);
+			if map.visible_tiles[idx] { ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph) }
 		}
 	}
 }
@@ -59,6 +61,7 @@ fn main() -> rltk::BError {
 
 	let map: Map = Map::new_map_rooms_and_corridors();
 	let (player_x, player_y) = map.rooms[0].center();
+	let rooms = map.rooms.clone();
 	gs.ecs.insert(map);
 
 	gs.ecs
@@ -72,6 +75,20 @@ fn main() -> rltk::BError {
 	  .with(Player {})
 	  .with(Viewshed { visible_tiles: Vec::new(), range: 8, dirty: true })
 	  .build();
+
+	for room in rooms.iter().skip(1) {
+		let (x,y) = room.center();
+		gs.ecs.create_entity()
+		  .with(Position{ x, y })
+		  .with(Renderable{
+			  glyph: rltk::to_cp437('g'),
+			  fg: RGB::named(rltk::RED),
+			  bg: RGB::named(rltk::BLACK),
+		  })
+		  .with(Viewshed{ visible_tiles : Vec::new(), range: 8, dirty: true })
+		  .build();
+	}
+
 
 	rltk::main_loop(context, gs)
 }
